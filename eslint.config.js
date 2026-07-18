f9b37bb8aaa85f8ad5b31da5e23791dc1e540577
hacxy <hacxy.js@outlook.com>
2026-07-18 13:10:32 +0800

    fix: 修复 ESLint tsconfigRootDir 配置

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,21 @@ import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, prettier, {
-  ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/generated/**'],
-})
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/generated/**', '**/prisma/**'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.js'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
+)
